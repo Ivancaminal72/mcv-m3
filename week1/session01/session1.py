63f4f906986057bbc8897d6e4e37f9bab9e6f2ca
Ivan Caminal <ivancaminal72@gmail.com>
2017-12-15 22:23:58 +0100

    create getImageDescriptors

--- week1/session01/session1.py
+++ week1/session01/session1.py
@@ -10,7 +10,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import ShuffleSplit
 
-features = "hist" #SIFT/hist
+features = "SIFT" #SIFT/hist
 agregate_sift_desc=True
 nfeatures = 100
 loadimages = 30
@@ -41,7 +41,6 @@ def inputImagesLabels():
     return train_images_filenames, test_images_filenames, train_labels, test_labels
 
 def SIFTfeatures(filenames,labels):
-    SIFTdetector = cv2.SIFT(nfeatures=nfeatures)
     descriptors = []
     label_per_descriptor = []
 
@@ -51,6 +50,7 @@ def SIFTfeatures(filenames,labels):
             # print 'Reading image ' + filename
             ima = cv2.imread(filename)
             gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
+            SIFTdetector = cv2.SIFT(nfeatures=nfeatures)
             kpt, des = SIFTdetector.detectAndCompute(gray, None)
             if agregate_sift_desc:
                 d = np.zeros([1, nfeatures * 128])
@@ -77,32 +77,52 @@ def histfeatures(filenames,labels):
             label_per_descriptor.append(labels[i])
     return descriptors,label_per_descriptor
 
-def featureExtraction(train_images_filenames, train_labels):
-    Train_descriptors=[]
-    Train_label_per_descriptor=[]
-    if features == "SIFT":
-        Train_descriptors, Train_label_per_descriptor = SIFTfeatures(train_images_filenames, train_labels)
-    elif features == "hist":
-        Train_descriptors, Train_label_per_descriptor = histfeatures(train_images_filenames, train_labels)
-    else:
-        print('feature specified not found')
-        exit(-1)
-    print 'Features extracted'
+def getImageDescriptors(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    if features == "hist":
+        hist, bin_edges = np.histogram(gray, 10)
+        return hist
+    elif features == "SIFT":
+        SIFTdetector = cv2.SIFT(nfeatures=nfeatures)
+        kpt, des = SIFTdetector.detectAndCompute(gray, None)
+
+        if agregate_sift_desc:
+            d = np.zeros([1, nfeatures * 128])
+            d[0, 0:des.size] = des[0:nfeatures, 0:128].reshape(1, -1)
+            return list(d)
+
+        return des
+
+
+
+def featureExtraction(filenames, labels):
+    descriptors = []
+    label_per_descriptor = []
+
+    # Load images and get its descriptors
+    for i in range(len(filenames)):
+        filename = filenames[i]
+        if label_per_descriptor.count(labels[i]) < loadimages:
+            # print 'Reading image ' + filename
+            image = cv2.imread(filename)
+            descriptors.append(getImageDescriptors(image))
+            label_per_descriptor.append(labels[i])
+
     # Transform everything to numpy arrays
     if features == "SIFT" and not agregate_sift_desc:
-        D = Train_descriptors[0]
-        L = np.array([Train_label_per_descriptor[0]] * Train_descriptors[0].shape[0])
+        D = descriptors[0]
+        L = np.array([label_per_descriptor[0]] * descriptors[0].shape[0])
 
-        for i in range(1, len(Train_descriptors)):
-            D = np.vstack((D, Train_descriptors[i]))
-            L = np.hstack((L, np.array([Train_label_per_descriptor[i]] * Train_descriptors[i].shape[0])))
+        for i in range(1, len(descriptors)):
+            D = np.vstack((D, descriptors[i]))
+            L = np.hstack((L, np.array([label_per_descriptor[i]] * descriptors[i].shape[0])))
     else:
-        D = np.array(Train_descriptors[0])
-        L = np.array(Train_label_per_descriptor[0])
+        D = np.array(descriptors[0])
+        L = np.array(label_per_descriptor[0])
 
-        for i in range(1, len(Train_descriptors)):
-            D = np.vstack((D, np.array(Train_descriptors[i])))
-            L = np.hstack((L, np.array(Train_label_per_descriptor[i])))
+        for i in range(1, len(descriptors)):
+            D = np.vstack((D, np.array(descriptors[i])))
+            L = np.hstack((L, np.array(label_per_descriptor[i])))
     return D, L
 
 
@@ -136,7 +156,7 @@ def predictAndTest( myknn,Test_descriptors,Test_label_per_descriptor):
         predictedclass = values[np.argmax(counts)]
         #print 'image ' + test_images_filenames[i] + ' was from class ' + test_labels[i] + ' and was predicted ' + predictedclass
         numtestimages += 1
-        PredictList.append(GetKey(predictedclass))
+        PredictList.append(predictedclass)
         if predictedclass == Test_label_per_descriptor[i]:
             numcorrect += 1
     npPredictList = np.array(PredictList)
@@ -237,7 +257,7 @@ def __main__():
         myknn = trainClassifier(D, L, k)
         accuracy,PredictList = predictAndTest(myknn,Test_descriptors,Test_label_per_descriptor)
         kPredictions.append(PredictList)
-        print ('for K = '+ str(k) + 'accuracy is ' + str(accuracy))
+        print ('for K = '+ str(k) + ' accuracy is ' + str(accuracy))
 
     evaluation(L,kPredictions)
     #print 'Final accuracy: ' + str(accuracy)

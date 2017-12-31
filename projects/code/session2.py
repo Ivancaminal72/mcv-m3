f4c68322ff34ff68f7dae41a4033e7ee92bcbf6c
AlexVicenteS <AlexVicenteS>
2017-12-31 19:32:47 +0100

    modularized code

--- projects/code/session2.py
+++ projects/code/session2.py
@@ -5,94 +5,112 @@ import time
 from sklearn.preprocessing import StandardScaler
 from sklearn import svm
 from sklearn import cluster
+# import matplotlib.pyplot as plt
 
-start = time.time()
 
-# read the train and test files
-train_images_filenames = cPickle.load(open('train_images_filenames.dat','r'))
-test_images_filenames = cPickle.load(open('test_images_filenames.dat','r'))
-train_labels = cPickle.load(open('train_labels.dat','r'))
-test_labels = cPickle.load(open('test_labels.dat','r'))
-print 'Loaded '+str(len(train_images_filenames))+' training images filenames with classes ',set(train_labels)
-print 'Loaded '+str(len(test_images_filenames))+' testing images filenames with classes ',set(test_labels)
+def inputImagesLabels():
+    # read the train and test files
+    train_images_filenames = cPickle.load(open('train_images_filenames.dat', 'r'))
+    test_images_filenames = cPickle.load(open('test_images_filenames.dat', 'r'))
+    train_labels = cPickle.load(open('train_labels.dat', 'r'))
+    test_labels = cPickle.load(open('test_labels.dat', 'r'))
+    print 'Loaded ' + str(len(train_images_filenames)) + ' training images filenames with classes ', set(train_labels)
+    print 'Loaded ' + str(len(test_images_filenames)) + ' testing images filenames with classes ', set(test_labels)
+    return train_images_filenames, test_images_filenames, train_labels, test_labels
 
-# create the SIFT detector object
-SIFTdetector = cv2.SIFT(nfeatures=300)
 
-# extract SIFT keypoints and descriptors
-# store descriptors in a python list of numpy arrays
-Train_descriptors = []
-Train_label_per_descriptor = []
-for i in range(len(train_images_filenames)):
-	filename=train_images_filenames[i]
-	print 'Reading image '+filename
-	ima=cv2.imread(filename)
-	gray=cv2.cvtColor(ima,cv2.COLOR_BGR2GRAY)
-	kpt,des=SIFTdetector.detectAndCompute(gray,None)
-	Train_descriptors.append(des)
-	Train_label_per_descriptor.append(train_labels[i])
-	print str(len(kpt))+' extracted keypoints and descriptors'
+def SIFTextraction(filenames,labels=[]):
+    # create the SIFT detector object
+    SIFTdetector = cv2.SIFT(nfeatures=300)
 
-# Transform everything to numpy arrays
-size_descriptors=Train_descriptors[0].shape[1]
-D=np.zeros((np.sum([len(p) for p in Train_descriptors]),size_descriptors),dtype=np.uint8)
-startingpoint=0
-for i in range(len(Train_descriptors)):
-	D[startingpoint:startingpoint+len(Train_descriptors[i])]=Train_descriptors[i]
-	startingpoint+=len(Train_descriptors[i])
+    # extract SIFT keypoints and descriptors
+    # store descriptors in a python list of numpy arrays
+    descriptors = []
+    label_per_descriptor = []
+    for i in range(len(filenames)):
+        filename = filenames[i]
+        print 'Reading image ' + filename
+        ima = cv2.imread(filename)
+        gray = cv2.cvtColor(ima, cv2.COLOR_BGR2GRAY)
+        kpt, des = SIFTdetector.detectAndCompute(gray, None)
+        descriptors.append(des)
+        if len(labels)!=0:
+            label_per_descriptor.append(labels[i])
+        print str(len(kpt)) + ' extracted keypoints and descriptors'
 
-#compute the codebook
-k = 512
-print 'Computing kmeans with '+str(k)+' centroids'
-init=time.time()
-codebook = cluster.MiniBatchKMeans(n_clusters=k, verbose=False, batch_size=k * 20,compute_labels=False,reassignment_ratio=10**-4,random_state=42)
-codebook.fit(D)
-cPickle.dump(codebook, open("codebook.dat", "wb"))
-end=time.time()
-print 'Done in '+str(end-init)+' secs.'
+    # Transform everything to numpy arrays
+    size_descriptors = descriptors[0].shape[1]
+    D = np.zeros((np.sum([len(p) for p in descriptors]), size_descriptors), dtype=np.uint8)
+    startingpoint = 0
+    for i in range(len(descriptors)):
+        D[startingpoint:startingpoint + len(descriptors[i])] = descriptors[i]
+        startingpoint += len(descriptors[i])
+    return D,descriptors,label_per_descriptor
 
-# get train visual word encoding
-print 'Getting Train BoVW representation'
-init=time.time()
-visual_words=np.zeros((len(Train_descriptors),k),dtype=np.float32)
-for i in xrange(len(Train_descriptors)):
-	words=codebook.predict(Train_descriptors[i])
-	visual_words[i,:]=np.bincount(words,minlength=k)
-end=time.time()
-print 'Done in '+str(end-init)+' secs.'
 
-# Train an SVM classifier with RBF kernel
-print 'Training the SVM classifier...'
-init=time.time()
-stdSlr = StandardScaler().fit(visual_words)
-D_scaled = stdSlr.transform(visual_words)
-clf = svm.SVC(kernel='rbf', C=1,gamma=.002).fit(D_scaled, train_labels)
-end=time.time()
-print 'Done in '+str(end-init)+' secs.'
+def computeCodebook(D,k=512):
+    # compute the codebook
+    print 'Computing kmeans with ' + str(k) + ' centroids'
+    init = time.time()
+    codebook = cluster.MiniBatchKMeans(n_clusters=k, verbose=False, batch_size=k * 20, compute_labels=False,
+                                       reassignment_ratio=10 ** -4, random_state=42)
+    codebook.fit(D)
+    cPickle.dump(codebook, open("codebook.dat", "wb"))
+    end = time.time()
+    print 'Done in ' + str(end - init) + ' secs.'
+    return codebook
 
-# get all the test data 
-print 'Getting Test BoVW representation'
-init=time.time()
-visual_words_test=np.zeros((len(test_images_filenames),k),dtype=np.float32)
-for i in range(len(test_images_filenames)):
-	filename=test_images_filenames[i]
-	print 'Reading image '+filename
-	ima=cv2.imread(filename)
-	gray=cv2.cvtColor(ima,cv2.COLOR_BGR2GRAY)
-	kpt,des=SIFTdetector.detectAndCompute(gray,None)
-	words=codebook.predict(des)
-	visual_words_test[i,:]=np.bincount(words,minlength=k)
-end=time.time()
-print 'Done in '+str(end-init)+' secs.'
+def getWords(codebook,descriptors,k=512):
+    # get train visual word encoding
+    print 'Getting BoVW representation'
+    init = time.time()
+    visual_words = np.zeros((len(descriptors), k), dtype=np.float32)
+    for i in xrange(len(descriptors)):
+        words = codebook.predict(descriptors[i])
+        visual_words[i, :] = np.bincount(words, minlength=k)
+    end = time.time()
+    print 'Done in ' + str(end - init) + ' secs.'
 
-# Test the classification accuracy
-print 'Testing the SVM classifier...'
-init=time.time()
-accuracy = 100*clf.score(stdSlr.transform(visual_words_test), test_labels)
-end=time.time()
-print 'Done in '+str(end-init)+' secs.'
-print 'Final accuracy: ' + str(accuracy)
+    return words,visual_words
 
-end=time.time()
-print 'Everything done in '+str(end-start)+' secs.'
-### 69.02%
+
+def trainSVM(visual_words,train_labels):
+    # Train an SVM classifier with RBF kernel
+    print 'Training the SVM classifier...'
+    init = time.time()
+    stdSlr = StandardScaler().fit(visual_words)
+    D_scaled = stdSlr.transform(visual_words)
+    clf = svm.SVC(kernel='rbf', C=1, gamma=.002).fit(D_scaled, train_labels)
+    end = time.time()
+    print 'Done in ' + str(end - init) + ' secs.'
+    return clf,stdSlr
+
+
+def evaluateAccuracy(clf,stdSlr,visual_words_test,test_labels):
+    # Test the classification accuracy
+    print 'Testing the SVM classifier...'
+    init = time.time()
+    accuracy = 100 * clf.score(stdSlr.transform(visual_words_test), test_labels)
+    end = time.time()
+    print 'Done in ' + str(end - init) + ' secs.'
+    print 'Final accuracy: ' + str(accuracy)
+    #return accuracy
+
+def __main__():
+    start = time.time()  # global time
+
+    train_images_filenames, test_images_filenames, train_labels, test_labels=inputImagesLabels() #get images sets
+    D, train_descriptors, label_per_descriptor=SIFTextraction(train_images_filenames, train_labels) #get SIFT descriptors for train set
+    k = 512
+    codebook=computeCodebook(D,k) #create codebook using train SIFT descriptors
+    train_words, train_visual_words=getWords(codebook,train_descriptors,k) #assign descriptors to nearest word(features cluster) in codebook
+    clf,stdSlr=trainSVM(train_visual_words,train_labels) #train SVM with with labeled visual words
+    D, test_descriptors, foo=SIFTextraction(test_images_filenames) #get SIFT descriptors for test set
+    test_words, test_visual_words=getWords(codebook,test_descriptors) #words found at test set
+    evaluateAccuracy(clf, stdSlr, test_visual_words,test_labels)
+
+    end = time.time()
+    print 'Everything done in ' + str(end - start) + ' secs.'
+    ### 69.02%
+
+__main__()
